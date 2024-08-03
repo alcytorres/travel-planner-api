91efefa2189eaf36cb7867e38c6714330d8494c2
Alcy Torres <mxalcytorres@gmail.com>
2024-08-03 03:13:11 -0400

    Add UserTrip model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_03_065657) do
+ActiveRecord::Schema[7.1].define(version: 2024_08_03_071102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_03_065657) do
     t.integer "year"
     t.text "highlights"
     t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_trips", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "trip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
