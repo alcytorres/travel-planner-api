095220412b6709271c16d0bd96520d371fc97ee5
Alcy Torres <mxalcytorres@gmail.com>
2024-08-06 13:03:29 -0400

    Indent user and trip models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-has_secure_password
-validates :email, presence: true, uniqueness: true
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
 
-has_many :user_trips
-has_many :trips, through: :user_trips
+  has_many :user_trips
+  has_many :trips, through: :user_trips
 end
